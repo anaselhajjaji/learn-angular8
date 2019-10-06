c0e6a8fc64747a9047f4c4482fb086021e30fc67
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2019-10-06 09:12:50 -0400

    55. Creating an "Ingredient" Model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    /*
+    This can be replaced by adding accessor in constructor
+    public name:string;
+    public amount:number;*/
+    constructor(public name: string, public amount: number) {
+        /*
+        This can be replaced by adding accessor in constructor
+        this.name = name;
+        this.amount = amount;*/
+    }
+}
