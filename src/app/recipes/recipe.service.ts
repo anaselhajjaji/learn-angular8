d31524b30720e2c01d7c74c449b8220d37918b0b
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2019-10-09 14:02:56 -0400

    117. Using a Service for Cross-Component Communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,18 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
-    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe',
-          'This is a description',
-          'https://www.bakedbyanintrovert.com/wp-content/uploads/2017/01/Basic-Vanilla-Cake_640-3.jpg'),
-        new Recipe('Another Test Recipe',
-          'This is a description',
-          'https://www.bakedbyanintrovert.com/wp-content/uploads/2017/01/Basic-Vanilla-Cake_640-3.jpg')
-      ];
+  
+  recipeSelected = new EventEmitter<Recipe>();
+  
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is a description',
+      'https://www.bakedbyanintrovert.com/wp-content/uploads/2017/01/Basic-Vanilla-Cake_640-3.jpg'),
+    new Recipe('Another Test Recipe',
+      'This is a description',
+      'https://www.bakedbyanintrovert.com/wp-content/uploads/2017/01/Basic-Vanilla-Cake_640-3.jpg')
+  ];
 
     getRecipes() {
         return this.recipes.slice(); // to return a copy to avoid returning the reference to the recipe property.
