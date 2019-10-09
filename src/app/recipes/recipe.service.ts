01b19900a7cfd5c228cf3696b7d2b686f3f4d96c
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2019-10-09 14:23:13 -0400

    120. Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   
@@ -8,10 +9,12 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe',
       'This is a description',
-      'https://www.bakedbyanintrovert.com/wp-content/uploads/2017/01/Basic-Vanilla-Cake_640-3.jpg'),
+      'https://www.bakedbyanintrovert.com/wp-content/uploads/2017/01/Basic-Vanilla-Cake_640-3.jpg',
+      [new Ingredient('Chocolate', 1), new Ingredient('Sugar', 20)]),
     new Recipe('Another Test Recipe',
       'This is a description',
-      'https://www.bakedbyanintrovert.com/wp-content/uploads/2017/01/Basic-Vanilla-Cake_640-3.jpg')
+      'https://www.bakedbyanintrovert.com/wp-content/uploads/2017/01/Basic-Vanilla-Cake_640-3.jpg',
+      [new Ingredient('Vanilla', 2), new Ingredient('Apple', 1)])
   ];
 
     getRecipes() {
