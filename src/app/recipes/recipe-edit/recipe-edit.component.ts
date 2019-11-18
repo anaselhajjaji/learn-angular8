2489cd6e501bef86d7c4e2603104e356d0156e5f
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2019-11-18 15:03:36 -0500

    230. Adding new Ingredient Controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -65,4 +65,13 @@ export class RecipeEditComponent implements OnInit {
   onSubmit() {
     console.log(this.recipeForm);
   }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(), 
+        'amount': new FormControl()
+      })
+    );
+  }
 }
