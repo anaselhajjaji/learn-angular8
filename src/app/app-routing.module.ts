4a1cab4971b377dfe06b2516183106feff7df8a2
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2020-02-18 12:12:43 -0500

    333. Preloading Lazy-Loaded Code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
-    { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule' },
-    { path: 'auth', loadChildren: './auth/auth.module#AuthModule' }
+    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' }, // Lazy loaded module
+    { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule' }, // Lazy loaded module
+    { path: 'auth', loadChildren: './auth/auth.module#AuthModule' } // Lazy loaded module
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)], // forRoot should be present only in the AppModule, for children we must use forChild()
+    imports: [RouterModule.forRoot(appRoutes/*to configure preloading strategy , { preloadingStrategy: PreloadAllModules }*/)], // forRoot should be present only in the AppModule, for children we must use forChild()
     exports: [RouterModule]
 })
 export class AppRoutingModule {
